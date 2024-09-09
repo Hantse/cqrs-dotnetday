ec8026c87d1f6ed7fa28781a1afee7bf0d9b81f0
PGrasseels <grasseelsp@hotmail.com>
2024-09-09 16:56:32 +0200

    Clean project

--- src/OrderService/OrderService.AppHost/Program.cs
+++
@@ -1,9 +0,0 @@
-var builder = DistributedApplication.CreateBuilder(args);
-
-var apiService = builder.AddProject<Projects.OrderService_ApiService>("apiservice");
-
-builder.AddProject<Projects.OrderService_Web>("webfrontend")
-    .WithExternalHttpEndpoints()
-    .WithReference(apiService);
-
-builder.Build().Run();
